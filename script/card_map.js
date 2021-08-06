dc7c8bb5ef1e6eb2ebc1184afa52b59cffa9cbeb
sallylin <missikun.tw@gmail.com>
2021-08-07 00:22:05 +0800

    fixed

--- script/card_map.js
+++ script/card_map.js
@@ -134,33 +134,15 @@ var ROAD_DIRECTION_MAP = {
     left: false,
     bottom: true,
   },
-  100:{
-    top: true,
-    right: true,
-    left: true,
-    bottom: true,
-  },
-  200:{
-    top: true,
-    right: false,
-    left: true,
-    bottom: false,
-  },
-  300:{
-    top: true,
-    right: true,
-    left: false,
-    bottom: false,
-  },
 };
+
 
 function CheckPosition(blockid) {
   const blockidArray = blockid.match(/\d/g);
   const row = blockidArray[0];
   const column = blockidArray[1];
-  // console.log("hi",row,column);
-  const targetBlock = blockid;
-  // console.log("targetBlock",targetBlock)
+  const targetBlock = blockid
+  
   const topBlockId = `block${row - 1}-${column}`;
   const rightBlockId = `block${row}-${column - -1}`;
   const leftBlockId = `block${row}-${column - 1}`;
@@ -176,24 +158,6 @@ function CheckPosition(blockid) {
   const Rightblock_left = $(`.${rightBlockId} img`).attr("left");
   const Leftblock_right = $(`.${leftBlockId} img`).attr("right");
   const Bottomblock_top = $(`.${bottomBlockId} img`).attr("top");
-  // console.log("四處位置",Topblock_bottom,Rightblock_left,Leftblock_right,Bottomblock_top)
-
-  // const deg = $(".redborder").attr("style").match(/\d+/gi);
-  // console.log("deg=", deg);
-  // console.log("fefwefewfewfewfewfew", $(".redborder").attr("style"));
-  // if (($(".redborder").attr("style").match(/\d+/gi) / 180) % 2 == 1) {
-  //   const topValue = object.attr("top");
-  //   const RightValue = object.attr("right");
-  //   const LeftValue = object.attr("left");
-  //   const BottomValue = object.attr("bottom");
-
-  //   object.attr("bottom", topValue);
-  //   object.attr("left", RightValue);
-  //   object.attr("right", LeftValue);
-  //   object.attr("top", BottomValue);
-  //   console.log("安安看有沒有變");
-  // }
-
 
   if (
     (target_Top == "true" && Topblock_bottom == "false") ||
@@ -212,43 +176,21 @@ function CheckPosition(blockid) {
     alert("白癡喔不能這樣放辣");
     return;
   }
-  $(".topcue").text("你剛剛放了一張道路卡～");
+  // let handCards = [];
+  // handCards = pools.splice(0, 6)
+  // for (let index = 0; index < 6; index++) {
+  //   const cardNumber = handCards[index];
+  //   giveDirection(index, cardNumber);
+  // }
+  
+  // if (cardNumber >= 1 && cardNumber <= 31) {
+  //   $(".topcue").text("你放了一張道路卡");
+  // }
+  // if (cardNumber >= 32 && cardNumber <= 40) {
+  //   $(".topcue").text("你放了一張坑路ＲＲＲ");
+  // }
   printNewCard();
   giveNewCard();
 
-  //target_Top上方判斷 可以的
-  //if target_Top == false (bigT horizontal downRight 其他坑洞卡
-  //&& Topblock_bottom == true
-  //target_left == true && Leftblock_right == true
-  //or
-  //target_right == true && Rightblock_left == true
-
-  //target_Top上方判斷 不行的
-  //if target_Top == false && Topblock_bottom == true -> return
-  //
-  //if target_Top == false && target_Right == false Leftblock_right == false || Rightblock_left == false -> return
-  //
-  //
-  //Topblock_bottom  == true
-
-  //target_Top == true
-  //Topblock_bottom  == true
-
-  //target_right == true
-  //Rightblock_left == true || undifined
-  //or
-  //target_right == false
-  //Rightblock_left == undifined
-
-  //target_left == true || false 都沒差
-  //Leftblock_right == true || undifined
-
-  //target_Bottom == true
-  //Bottomblock_top == true || undifined
-  //or
-  //target_Bottom == false
-  //Bottomblock_top == undifined
-
-  //左方判斷
-  //target_Top == true  || undifined
+  
 }
